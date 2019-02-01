3110df6c4cdb441be44b60e3f94127777da59054
vivekumarsingh <46886866+vivekumarsingh@users.noreply.github.com>
2019-02-01 18:12:21 +0530

    Add new files by creating new json
    Hit the url of rest_countries and get the response of name of regionalblocs

---
+++ json.py
@@ -0,0 +1,10 @@
+import requests
+import json
+r = requests.get("https://restcountries.eu/rest/v2/region/europe")
+print(r.text)
+for a in r.json():
+    r = a['regionalBlocs']   #("this is array")
+for b in r:
+    print(b['name'])
+    for a in r.json():    #(" this is dictionary")
+        print(a['translations']['de'])
